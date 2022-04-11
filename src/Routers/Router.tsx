e850063ef8df8c4fed1dd6b537729665da633a02
unknown <crsn1111@gmail.com>
2022-04-11 16:38:52 +0900

    hashRouter

--- src/Routers/Router.tsx
+++ src/Routers/Router.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import AuthForm from "../Pages/AuthForm";
 import TodoList from "../Pages/TodoList";
 import Header from "./Header";
 
 const Router = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<TodoList />} />
         <Route path="/authForm" element={<AuthForm />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
